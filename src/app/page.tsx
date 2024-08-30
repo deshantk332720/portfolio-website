31e2e1636a45f7e255c65e7e1d3ae7e348826284
deshant <163555713+deshantk332720@users.noreply.github.com>
2024-08-30 14:18:20 +0500

    push my code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,83 @@
-import Image from "next/image";
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+  <div>
+      
+      <div className=" hidden md:block">
+        <nav className="p-4">
+          <ul className="flex justify-end item-center gap-40">
+            <li><Link href="#About Me">About </Link></li>
+            <li><Link href="#Future Plan">Future Plan</Link></li>
+            <li><Link href="#Goals and Aspirations">Goals and Aspirations</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="rounded-full overflow-hidden w-48 h-49 justify-content:center flex  align-items:center">
+        <img src="deshantkumar.png"></img>
+      </div>
+
+      <div>
+        <h1> Hi,I'm <b className="text-blue-900"> Deshant Kumar</b></h1><br />
+        <p className="align-center flex justify-center text-2xl">Welcome to my portfolio.Here you will find more information About Me,what can i do,and my current skills mostly in terms of programming and technology.</p>
+      </div>
+
+      <br />
+      <div>
+        <h1>Connect with Me</h1>
+      </div>
+
+      <nav className="bg-black text-white p-3">
+        <div className="container mx-auto flex justify-between items-center">
+          
+          <a href="https://github.com/deshantk332720/deshant" target="_blank" className="w-10 h-10">
+            <img className="w-10 h-10" src="/github.png" alt="GitHub" />
+          </a>
+          
+          <a href="https://www.linkedin.com/feed/" target="_blank" className="w-10 h-10">
+            <img className="w-10 h-10" src="/linkedin.png" alt="LinkedIn" />
+          </a>
+          
+          <a href="https://www.npmjs.com/~deshant_kumarrathi3327" target="_blank" className="w-10 h-10">
+            <img className="w-10 h-10" src="/npm.png" alt="NPM" />
           </a>
         </div>
+      </nav>
+      <br />
+      <br />
+
+
+
+
+
+      <section id="About Me">
+        <h1><b>About Me</b></h1>
+        <p><b>My name is Deshant Kumar. I am the student of grade 10</b>, residing in Karachi, Pakistan.I have been passionate about technology and programming. This passion has driven me to pursue learning beyond the standard curriculum.</p>
+        <p> Alongside my school studies, I am actively learning and working with several programming languages and frameworks. I am currently focused on mastering Next.js, a popular React framework for building fast and dynamic web applications. Additionally, I am expanding my knowledge of Python, a versatile language that is essential for various programming tasks and data analysis. My foundation in JavaScript helps me create interactive and engaging web experiences. </p>
+      </section>
+      <br />
+      <section id="Future Plan">
+        <h1><b>Future Plan</b></h1>
+        <p>My future plans include continuing my education in computer science and technology, working on real-world projects, and gaining practical experience. I am committed to fulfilling my dreams and making a significant contribution to the tech industry.</p>
+      </section>
+      <br />
+      <section id="Goals and Aspirations">
+        <h1><b>Goals and Aspirations</b></h1>
+        <p><b>My aspiration is to become a proficient Full Stack Developer and Web Developer</b>. I am dedicated to acquiring a diverse skill set in both frontend and backend technologies. By pursuing this path, I aim to build innovative and impactful web solutions.</p>
+      </section>
+      <div>
+        <title>
+        <b>Deshant Kumar</b>
+        </title>
       </div>
+      
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
 }
+
+
